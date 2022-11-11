fe0e2a70c48134b872428c5232e839b016e6adc0
Nikola1709 <savickosovska@gmail.com>
2022-11-11 23:52:40 +0100

    Created GUI for passenger and driver main menu

---
+++ AndroidApp/UberApp_Tim12/app/src/main/java/com/example/uberapp_tim12/PassengerRideHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.uberapp_tim12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PassengerRideHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_ride_history);
+    }
+}
