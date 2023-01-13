4721d7f18aebaa1a4d60c7b6d9333bcf9f8d768c
serbedzijajovan <jovajoksa@gmail.com>
2023-01-13 17:38:34 +0100

    Added statistic activity for passenger

--- AndroidApp/UberApp_Tim12/app/src/main/java/com/example/uberapp_tim12/activities/PassengerSettingsActivity.java
+++ AndroidApp/UberApp_Tim12/app/src/main/java/com/example/uberapp_tim12/activities/PassengerSettingsActivity.java
@@ -21,7 +21,7 @@ public class PassengerSettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger_settings);
 
-        this.getWindow().setStatusBarColor(this.getResources().getColor(R.color.black,this.getTheme()));
+        this.getWindow().setStatusBarColor(this.getResources().getColor(R.color.black, this.getTheme()));
         Toolbar toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -35,6 +35,17 @@ public class PassengerSettingsActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(PassengerSettingsActivity.this, PassengerAccountActivity.class);
+                User user = UserMockup.getUser();
+                intent.putExtra("user", user);
+                startActivity(intent);
+            }
+        });
+
+        ConstraintLayout reportLayout = findViewById(R.id.show_reports_passenger);
+        reportLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PassengerSettingsActivity.this, PassengerStatisticActivity.class);
                 User user = UserMockup.getUser();
                 intent.putExtra("user", user);
                 startActivity(intent);
---
+++ AndroidApp/UberApp_Tim12/app/src/main/java/com/example/uberapp_tim12/activities/PassengerStatisticActivity.java
@@ -0,0 +1,80 @@
+package com.example.uberapp_tim12.activities;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.uberapp_tim12.R;
+import com.example.uberapp_tim12.fragments.PassengerStatisticRideCostFragment;
+import com.example.uberapp_tim12.fragments.PassengerStatisticRideCountFragment;
+import com.example.uberapp_tim12.fragments.PassengerStatisticRideDistanceFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.tabs.TabLayout;
+
+public class PassengerStatisticActivity extends AppCompatActivity {
+    BottomNavigationView statisticNavigation;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_statistic);
+
+        this.getWindow().setStatusBarColor(this.getResources().getColor(R.color.black, this.getTheme()));
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle("Statistic");
+
+        statisticNavigation = findViewById(R.id.bottom_statistic_navigation);
+        statisticNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getTitle().toString()) {
+                    case "Rides":
+                        changeToCountStatistic();
+                        return true;
+                    case "Distance":
+                        changeToDistanceStatistic();
+                        return true;
+                    case "Money":
+                        changeToMoneySpentStatistic();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        changeToCountStatistic();
+    }
+
+    private void changeToCountStatistic() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.statistic_fragment, new PassengerStatisticRideCountFragment());
+        ft.commit();
+    }
+
+    private void changeToDistanceStatistic() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.statistic_fragment, new PassengerStatisticRideDistanceFragment());
+        ft.commit();
+    }
+
+    private void changeToMoneySpentStatistic() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.statistic_fragment, new PassengerStatisticRideCostFragment());
+        ft.commit();
+    }
+
+}
