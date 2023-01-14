12227fff8393663df3d5bdd24c1591660e9d33a2
serbedzijajovan <jovajoksa@gmail.com>
2023-01-14 22:10:14 +0100

    Fixed errors

--- AndroidApp/UberApp_Tim12/app/src/main/java/com/example/uberapp_tim12/activities/UserLoginActivity.java
+++ AndroidApp/UberApp_Tim12/app/src/main/java/com/example/uberapp_tim12/activities/UserLoginActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,12 +16,12 @@ import android.widget.TextView;
 import com.example.uberapp_tim12.R;
 
 import java.sql.Driver;
+import java.util.Properties;
 
 public class UserLoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
         this.getWindow().setStatusBarColor(this.getResources().getColor(R.color.black,this.getTheme()));
